745bf0351a26fe989a6144164d016d2af365e51f
William Dario Quiceno <89299118+wquiceno1@users.noreply.github.com>
2021-11-04 15:12:14 -0500

    Creacion de la base html

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Blog William Quiceno</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Responsive navbar-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="#!">Blog William Quiceno</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link active" href="#">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Page header with logo and tagline-->
+        <header class="py-5 bg-light border-bottom mb-4">
+            <div class="container">
+                <div class="text-center my-5">
+                    <h1 class="fw-bolder">Bienvenido al inicio del blog.</h1>
+                    <p class="lead mb-0">Proyecto desarrollado en HTML/CSS/PHP/MYSQL</p>
+                </div>
+            </div>
+        </header>
+        <!-- Page content-->
+        <div class="container">
+            <div class="row">
+                <!-- Blog entries-->
+                <div class="container">          
+                    <h2 class="card-title">Agregar entradas</h2>
+                    <div class="card w-75">
+                        <form action="" method="post" enctype="multipart/form-data">
+                        <input type="text"  placeholder="titulo" id="Titulo"><br><br>
+                        <input type="email" name="email" id="email" placeholder="Email"><br><br>
+                        <input type="file" name="imagen" id="imagen"><br><br>
+                        <textarea class="form-control" id="contenido" rows="3" placeholder="Contenido" ></textarea><br><br>
+                        <input type="submit" class="btn btn-success"  value="Guardar">
+
+
+                        </form>
+                    </div>                 
+                         
+                    <br>
+                    <div class="row">
+                        <div class="col-sm-6">                            
+                            <h2 class="card-title">Featured Post Title</h2>
+                            <a href="#!"><img class="card-img-top" src="https://dummyimage.com/850x350/dee2e6/6c757d.jpg" alt="..." /></a>                                
+                        </div>
+                        <div class="col-sm-6">
+                        <div class="card-body">
+                                <div class="small text-muted">January 1, 2021</div>
+                                
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <br><br>
+                    <div class="row">
+                        <div class="col-sm-6">                            
+                            <h2 class="card-title">Featured Post Title</h2>
+                            <a href="#!"><img class="card-img-top" src="https://dummyimage.com/850x350/dee2e6/6c757d.jpg" alt="..." /></a>                                
+                        </div>
+                        <div class="col-sm-6">
+                        <div class="card-body">
+                                <div class="small text-muted">January 1, 2021</div>
+                                
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>                
+            </div>
+        </div>
+        <br><br>
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
